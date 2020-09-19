78b8ea2b82c1a9d4fc25a09ed44424deceea0597
qhduan <mail@qhduan.com>
2020-09-19 17:46:07 +0000

    lint

--- src/listeners/drive-response/keywords-generate.ts
+++ src/listeners/drive-response/keywords-generate.ts
@@ -1,14 +1,13 @@
 
-const nodejieba = require("nodejieba")
+const nodejieba = require('nodejieba')
 
-
-export default function keywordsGenerate(keywords: string) {
-    const cut: string[] = nodejieba.cutAll(keywords)
-    const words: string[] = []
-    for (const c of cut) {
-        if (words.indexOf(c) < 0) {
-            words.push(c)
-        }
+export default function keywordsGenerate (keywords: string) {
+  const cut: string[] = nodejieba.cutAll(keywords)
+  const words: string[] = []
+  for (const c of cut) {
+    if (words.indexOf(c) < 0) {
+      words.push(c)
     }
-    return words.map(k => `(name contains '${k}' or fullText contains '${k}')`).join(' or ')
+  }
+  return words.map(k => `(name contains '${k}' or fullText contains '${k}')`).join(' or ')
 }
