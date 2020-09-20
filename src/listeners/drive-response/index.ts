df40aba83f0f0a44238d3af7f5b8d4531d6fae13
段清华DEAN <qhduan@users.noreply.github.com>
2020-09-20 17:52:59 +0800

    Qhd (#11)
    * improve time
    
    * lint
    
    * 1.0.7
    
    * add words cut
    
    * lint
    
    * 1.0.8
    
    * improve time
    
    * lint
    
    * add words cut
    
    * lint
    
    * update yarn.lock
    
    * 0.0.11

--- src/listeners/drive-response/index.ts
+++ src/listeners/drive-response/index.ts
@@ -1,25 +1,35 @@
 import { Message } from 'wechaty'
 import parseDate from './parse-date'
 import parseNumber from './parse-number'
+import keywordsGenerate from './keywords-generate'
 
 interface DriveAction {
   action: 'search' | 'list' | 'reply',
   limit?: number,
   keywords?: string,
   date?: string,
+  dateRange?: string[],
   text?: string
 }
 
 const StopWords = new RegExp(`
 相关的文件
 相关的内容
+相关的文档
+相关文件
+相关内容
+相关文档
 的文件
 的内容
+的文档
 我想看
 搜索
 查看
 查找
+查询
 `.split(/\n/).map(x => x.trim()).filter(x => x).join('|'), 'g')
+const BeforeRe = /之前|前面|以前|早于/ig
+const AfterRe = /之后|后面|以后|晚于/ig
 
 export function matchResponse (text: string): DriveAction | null {
   const date = parseDate(text)
@@ -44,19 +54,26 @@ export function matchResponse (text: string): DriveAction | null {
     return ret
   }
 
-  if (/^(我想看|搜索|查看|查找)/i.test(text)) {
+  if (/^(我想看|搜索|查看|查找|查询)/i.test(text)) {
     let keywords
     if (date) {
       keywords = date.clean.replace(StopWords, '')
     } else {
       keywords = text.replace(StopWords, '')
     }
+    keywords = keywords.replace(/的/g, ' ').trim()
+    keywords = keywords.replace(BeforeRe, ' ').trim()
+    keywords = keywords.replace(AfterRe, ' ').trim()
     const ret: DriveAction = {
       action: 'search',
       keywords,
     }
     if (date) {
-      ret.date = date.date
+      if (date.date) {
+        ret.date = date.date
+      } else if (date.dateRange) {
+        ret.dateRange = date.dateRange
+      }
     }
     return ret
   }
@@ -64,19 +81,33 @@ export function matchResponse (text: string): DriveAction | null {
   return null
 }
 
-function actionToQuery (action: DriveAction): string|null {
+function actionToQuery (text: string, action: DriveAction): string|null {
   if (action.action === 'list') {
     return 'orderBy createdTime desc'
   }
   if (action.action === 'search' && action.keywords) {
     const keywords = action.keywords.replace("'", '')
-    return `name contains '${keywords}' or fullText contains '${keywords}'`
+    const ands: string[] = []
+    ands.push(keywordsGenerate(keywords))
+
+    if (action.dateRange) {
+      ands.push(`(createdTime >= '${action.dateRange[0]}T00:00:00' and createdTime <= '${action.dateRange[1]}T23:59:59')`)
+    } else if (action.date) {
+      if (AfterRe.test(text)) {
+        ands.push(`(createdTime >= '${action.date}T00:00:00')`)
+      } else if (BeforeRe.test(text)) {
+        ands.push(`(createdTime <= '${action.date}T23:59:59')`)
+      } else {
+        ands.push(`(createdTime >= '${action.date}T00:00:00' and createdTime <= '${action.date}T23:59:59')`)
+      }
+    }
+    return ands.join(' and ')
   }
   return null
 }
 
 export default async function driveResponse (message: Message) {
-  const text = message.text()
+  const text = await message.mentionText()
   const response = matchResponse(text)
   if (response) {
     if (response.action === 'reply') {
@@ -84,9 +115,9 @@ export default async function driveResponse (message: Message) {
         await message.say(response.text)
       }
     } else {
-      const query = actionToQuery(response)
+      const query = actionToQuery(text, response)
       if (query) {
-        await message.say(JSON.stringify(response))
+        await message.say(query)
       }
     }
   }
--- src/listeners/drive-response/parse-date.ts
+++ src/listeners/drive-response/parse-date.ts
@@ -7,19 +7,58 @@ const model = recognizer.getDateTimeModel()
 
 interface DateResult {
   clean: string,
-  date: string,
+  date?: string,
+  dateRange?: string[],
 }
 
+/**
+ * @param text
+ *   {
+      "start": 2,
+      "end": 5,
+      "resolution": {
+          "values": [
+              {
+                  "timex": "XXXX-09-12",
+                  "type": "date",
+                  "value": "2020-09-12"
+              },
+              {
+                  "timex": "XXXX-09-12",
+                  "type": "date",
+                  "value": "2021-09-12"
+              }
+          ]
+      },
+      "text": "9月12",
+      "typeName": "datetimeV2.date"
+  }
+ */
 export default function parseDate (text: string): DateResult|null {
   const result = model.parse(text)
   if (result && result.length) {
     const before = text.slice(0, result[0].start)
     const after = text.slice(result[0].end + 1)
 
-    return {
-      clean: before + after,
-      date: result[0].resolution.values[0].timex,
+    if (result[0].resolution.values[0].value) {
+      return {
+        clean: before + after,
+        date: result[0].resolution.values[0].value,
+      }
+    }
+
+    if (result[0].resolution.values[0].start) {
+      return {
+        clean: before + after,
+        dateRange: [
+          result[0].resolution.values[0].start,
+          result[0].resolution.values[0].end,
+        ],
+      }
     }
   }
   return null
 }
+
+// console.log(JSON.stringify(model.parse('查找9月12的汽车'), null, 4))
+// console.log(JSON.stringify(model.parse('查找上周微信生态的文件'), null, 4))
